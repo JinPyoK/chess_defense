fd80b4ddbf8127516feb92a8fc1fbbedffb4bff4
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-02-02 00:10:10 +0900

    core 파일

---
+++ lib/core/constant/asset_path.dart
@@ -0,0 +1,15 @@
+/// 이미지
+const imageAppIconPath = 'assets/image/app_icon.png';
+
+/// 사운드
+const soundGameStartPath = 'assets/sound/game_start.mp3';
+const soundPieceMovePath = 'assets/sound/piece_move.mp3';
+const soundPieceTapPath = 'assets/sound/piece_tap.mp3';
+const soundBlackKilledPath = 'assets/sound/black_killed.mp3';
+const soundWhiteKilledPath = 'assets/sound/white_killed.mp3';
+const soundExecuteCheckPath = 'assets/sound/execute_check.mp3';
+const soundSpawnQueenPath = 'assets/sound/spawn_queen.mp3';
+const soundSpawnRookPath = 'assets/sound/spawn_rook.mp3';
+const soundSpawnKnightPath = 'assets/sound/spawn_knight.mp3';
+const soundSpawnBishopPath = 'assets/sound/spawn_bishop.mp3';
+const soundSpawnPawnPath = 'assets/sound/spawn_pawn.mp3';
---
+++ lib/core/constant/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xffF5F5F5);
+
+const blackColor = Colors.black;
+
+const redColor = Colors.redAccent;
+
+const woodColor = Color(0xffDFB460);
+
+const inGameBlackColor = Color(0xff282C33);
