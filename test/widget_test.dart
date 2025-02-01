0b28d025e9791e49dda0e3108bd1c893b8cd8b34
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-02-01 21:00:27 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chess_defense/main.dart';
+import 'package:chess_defense/core/main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
