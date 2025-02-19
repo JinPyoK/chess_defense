3a7722f6e9dbdce701a1eb5307ff4dcaacdc906e
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-02-19 13:26:45 +0900

    정책
    1. 개인정보 처리방침 화면에서 뒤로가기 다이얼로그
    
    2. animateTo -> jumpTo 애니메이션으로 하면 랭킹 스크린이 잠깐 보이면서 의도치않게 데이터가 불러와짐
    
    3. 네비게이션 스크린 앱바와 분리하여 앱바만 setState 될 수 있도록

--- lib/ui/common/screen/main_navigation_screen.dart
+++ lib/ui/common/screen/main_navigation_screen.dart
@@ -26,20 +26,13 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   int _currentIndex = 0;
 
-  Future<void> _getGolds() async {
-    myGolds = await GoldRepository().getGolds();
-    setState(() {});
-  }
-
   @override
   void initState() {
     super.initState();
-    setStateGold = setState;
 
     globalContext = context;
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _getGolds();
       compareStoreVersionAndShowDialog(context);
     });
   }
@@ -56,68 +49,51 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       canPop: false,
       onPopInvokedWithResult: (_, __) {
         showCustomDialog(
-            context,
-            defaultAction: false,
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text("Are you sure you want to exit the game?"),
-                const SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey,
-                      ),
-                      onPressed: () {
-                        Navigator.of(context, rootNavigator: true).pop();
-                      },
-                      child: const Text("Cancel"),
+          context,
+          defaultAction: false,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Are you sure you want to exit the game?"),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
                     ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: redColor,
-                      ),
-                      onPressed: () {
-                        SystemNavigator.pop();
-                      },
-                      child: const Text("Exit"),
-                    )
-                  ],
-                )
-              ],
-            ));
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).pop();
+                    },
+                    child: const Text("Cancel"),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: redColor),
+                    onPressed: () {
+                      SystemNavigator.pop();
+                    },
+                    child: const Text("Exit"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: whiteColor,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: GoldWidget(gold: myGolds),
-            ),
-          ],
-        ),
+        appBar: const _CommonAppBar(),
         body: PageView(
           controller: _pageController,
           physics: const NeverScrollableScrollPhysics(),
-          children: const [
-            HomeScreen(),
-            RankingScreen(),
-            AdScreen(),
-          ],
+          children: const [HomeScreen(), RankingScreen(), AdScreen()],
         ),
         bottomNavigationBar: NavigationBar(
           selectedIndex: _currentIndex,
           onDestinationSelected: (index) {
             _currentIndex = index;
-            _pageController.animateToPage(
-              index,
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeOut,
-            );
+            _pageController.jumpToPage(index);
             setState(() {});
           },
           destinations: const <NavigationDestination>[
@@ -139,15 +115,52 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
             ),
             NavigationDestination(
               icon: Icon(Icons.live_tv_outlined),
-              selectedIcon: Icon(
-                Icons.live_tv_outlined,
-                color: whiteColor,
-              ),
+              selectedIcon: Icon(Icons.live_tv_outlined, color: whiteColor),
               label: "Ad",
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _CommonAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const _CommonAppBar();
+
+  @override
+  State<_CommonAppBar> createState() => _CommonAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CommonAppBarState extends State<_CommonAppBar> {
+  Future<void> _getGolds() async {
+    myGolds = await GoldRepository().getGolds();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setStateGold = setState;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getGolds();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: whiteColor,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 24),
+          child: GoldWidget(gold: myGolds),
+        ),
+      ],
     );
   }
 }
