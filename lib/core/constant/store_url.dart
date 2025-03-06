631b8db4a33c303451f890061cafeaafc3773003
JinPyoK <111625932+JinPyoK@users.noreply.github.com>
2025-03-06 17:30:38 +0900

    앱스토어 url 변경

--- lib/core/constant/store_url.dart
+++ lib/core/constant/store_url.dart
@@ -2,4 +2,4 @@ const androidPlayStoreUrl =
     'https://play.google.com/store/apps/details?id=com.jinpyok.chess_defense&gl=US';
 
 const iosAppStoreUrl =
-    'https://itunes.apple.com/lookup?bundleId=com.jinpyok.chess_defense';
+    'https://itunes.apple.com/lookup?bundleId=com.jinpyok.chessDefense';
